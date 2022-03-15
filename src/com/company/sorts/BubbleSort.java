c33ec277f113da4b25d4b4fc890a6f231dda61e4
PTMC7216 <94150909+PTMC7216@users.noreply.github.com>
2022-03-15 10:14:47 -0700

    added bubble sort

---
+++ src/com/company/sorts/BubbleSort.java
@@ -0,0 +1,23 @@
+package com.company.sorts;
+
+public class BubbleSort {
+
+    public int[] start(int[] arr) {
+        int index_len = arr.length - 1;
+        boolean loop = true;
+        while (loop) {
+            loop = false;
+            for (int i = 0; i < index_len; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    arr[i] =     arr[i] + arr[i + 1];
+                    arr[i + 1] = arr[i] - arr[i + 1];
+                    arr[i] =     arr[i] - arr[i + 1];
+                    loop = true;
+                }
+            }
+            index_len--;
+        }
+        return arr;
+    }
+
+}
